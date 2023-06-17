07616097cb7468254e806373a134fe2654ec55e8
root <root@DESKTOP-EG0SK6P.localdomain>
2023-06-16 22:22:57 -0300

    feat: jquery and blockUI imports

--- resources/views/pages/produtos/paginacao.blade.php
+++ resources/views/pages/produtos/paginacao.blade.php
@@ -6,12 +6,15 @@
         <h1 class="h2">Produtos</h1>
     </div>
     <div>
-        <form action="" method="get">
+        <form action="{{ route('produto.index')}}" method="get">
             <input type="text" name="pesquisar" placeholder="Digite o nome">
             <button>Pesquisar</button>
             <a type="button" href="" class="btn btn-success float-end"> Incluir Produto</a>
         </form>
         <div class="table-responsive mt-4">
+            @if ($findProduto->isEmpty())
+                <p>Não existem dados</p>
+            @else
             <table class="table table-striped table-sm">
             <thead>
                 <tr>
@@ -29,7 +32,7 @@
                             <a href="" class="btn btn-light btn-sm">
                                 Editar
                             </a>
-                            <a href="" class="btn btn-danger btn-sm">
+                            <a href=" {{ route('produto.delete')}}" class="btn btn-danger btn-sm">
                                 Excluir
                             </a>
                         </td>
@@ -38,6 +41,7 @@
             
             </tbody>
             </table>
+            @endif
         </div>
     </div>
 @endsection
